dad121885397fe2c146260f67c10e315dd2e5a44
Viktor Hercinger <herczy@balabit.hu>
2015-04-25 06:34:35 +0200

    skeleton project created

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="pygitmetric",
+    description="Git-aware code metrics for Python repositories",
+    long_description= """\
+Calculate some metrics of a Python code based on the git repository and
+the changes committed. This way we can calculate how a code base has changed.
+
+Some example metrics are:
+ * How often do specific classes and functions change?
+ * What functions change often together?
+ * How complex are the changes on a file?
+ * etc.
+
+PyGitMetric provides an API for writing small scripts to implement other
+metrics.
+""",
+    license="LGPLv2+",
+    version="1.0.0",
+    author="Viktor Hercinger",
+    author_email="viktor.hercinger@balabit.com",
+    maintainer="Viktor Hercinger",
+    maintainer_email="viktor.hercinger@balabit.com",
+    zip_safe=True,
+    use_2to3=False,
+    packages=['pygitmetric']
+)
